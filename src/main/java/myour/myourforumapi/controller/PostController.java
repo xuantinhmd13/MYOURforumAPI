028c459328249a21c9af509b3f23a90c02c528ab
Mạc Đỗ Xuân Tính <mdxt13tk@gmail.com>
2020-12-09 11:50:23 +0700

    complete delete post, reload post.

--- src/main/java/myour/myourforumapi/controller/PostController.java
+++ src/main/java/myour/myourforumapi/controller/PostController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -64,5 +65,15 @@ public class PostController {
     @PutMapping("/posts/{id}")
     public int updatePost(@RequestBody Post postEdited) {
         return postRepository.save(postEdited).getId();
+    }
+
+    @DeleteMapping("/posts/{id}")
+    public void deletePost(@RequestParam int id) {
+        postRepository.deleteById(id);
+    }
+
+    @GetMapping("/posts/{id}")
+    public Post getPostById(@RequestParam int id) {
+        return postRepository.findById(id).get();
     }
 }
